d13d7f89673920b87a02156f1763dcdbc2275de1
Douglas Barankievicz <douglasbarankdev@gmail.com>
2025-07-16 20:39:01 +0000

    add task solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,23 @@
-export const Tabs = () => {};
+
+export const Tabs = ({ pagina, idAtivo, setIdAtivo }) => {
+    return (
+        <div data-cy="TabsComponent">
+            <h1 className="title">Selected tab is {pagina.find(pag => pag.id === idAtivo)?.title}</h1>
+            <div className="tabs is-boxed">
+                <ul>
+                    {pagina.map((pag, index) => (
+                        <li
+                            onClick={(e) => {e.preventDefault(); setIdAtivo(pag.id)}}
+                            className={pag.id === idAtivo ? 'is-active' : ''}
+                            key={pag.id}
+                        >
+                            <a>{pag.title}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <p>{pagina.find(pag => pag.id === idAtivo)?.content}</p>
+        </div>
+    )
+};
